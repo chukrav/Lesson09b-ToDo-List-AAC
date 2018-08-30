7db7bc629247e052831cb715632eac985943b17c
lesha K <alexei.jobs@gmail.com>
2018-08-30 15:54:21 +0300

    Need to recapgit status! T09b.09-Exercise-AddTheViewModel

--- T09b.09-Exercise-AddTheViewModel/app/src/main/java/com/example/android/todolist/MainViewModel.java
+++ T09b.09-Exercise-AddTheViewModel/app/src/main/java/com/example/android/todolist/MainViewModel.java
@@ -1,11 +1,34 @@
 package com.example.android.todolist;
 
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import com.example.android.todolist.database.AppDatabase;
+import com.example.android.todolist.database.TaskEntry;
+
+import java.util.List;
+
 // TODO (1) make this class extend AndroidViewModel and implement its default constructor
-public class MainViewModel {
+public class MainViewModel extends AndroidViewModel {
+
+    private LiveData<List<TaskEntry>> mTasks;
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+//        mTasks = AppDatabase.getInstance(this.getApplication()).taskDao().loadAllTasks();
+        mTasks = AppDatabase.getInstance(application).taskDao().loadAllTasks(); //// ????!!!!
+    }
 
     // TODO (2) Add a tasks member variable for a list of TaskEntry objects wrapped in a LiveData
 
     // TODO (4) In the constructor use the loadAllTasks of the taskDao to initialize the tasks variable
 
     // TODO (3) Create a getter for the tasks variable
+    public LiveData<List<TaskEntry>> getTasks() {
+        return mTasks;
+    }
+
+
 }
